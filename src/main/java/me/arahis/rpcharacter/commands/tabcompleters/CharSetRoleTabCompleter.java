d48b8457fd6407c1113f0e5ba6136e0e591249bb
Tamerlan Kairbek <kairbektamerlan001@gmail.com>
2023-06-12 13:58:26 +0600

    feat: returning ids in tabcomplete

---
+++ src/main/java/me/arahis/rpcharacter/commands/tabcompleters/CharSetRoleTabCompleter.java
@@ -0,0 +1,50 @@
+package me.arahis.rpcharacter.commands.tabcompleters;
+
+import me.arahis.rpcharacter.RPCharacterPlugin;
+import me.arahis.rpcharacter.database.IDataHandler;
+import me.arahis.rpcharacter.utils.Refactor;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CharSetRoleTabCompleter implements TabCompleter {
+
+    private int amount;
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        RPCharacterPlugin plugin = RPCharacterPlugin.getPlugin();
+        IDataHandler dataHandler = plugin.getDataHandler();
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+            setAmount(dataHandler.getRPPlayer((Player) sender).getAmountOfChars());
+        });
+
+        List<String> ids = new ArrayList<>();
+
+        for(int i = 0; i <= getAmount(); i++) {
+            ids.add(String.valueOf(i));
+        }
+
+        if(args.length == 1) {
+            return Refactor.getPartialMatches(args[0], ids);
+        }
+
+        return null;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
